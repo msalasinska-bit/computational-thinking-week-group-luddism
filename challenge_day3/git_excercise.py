7f0b871779ad07c49fcccd50f8222000210949b4
josefinem12 <josymertens11@gmail.com>
2024-09-02 14:11:08 +0200

    did excrcise

---
+++ challenge_day3/git_excercise.py
@@ -0,0 +1,16 @@
+from marysia_salasinska import marysia 
+from file_david import david 
+from filejosy import josy 
+from filealva import alva 
+from fileadam import adam
+
+def group():
+    print("This is Team Luddism. We are:")
+    print(marysia())
+    print(david())
+    print(josy())
+    print(alva())
+    print(adam())
+
+group()
+
