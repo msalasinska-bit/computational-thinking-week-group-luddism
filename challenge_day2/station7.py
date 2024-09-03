6214e9cd3fb852396bd8225bbea087aa50f363c5
davidliebm <167301968+davidliebm@users.noreply.github.com>
2024-09-03 10:48:24 +0200

    Updated station 7

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,19 +1,21 @@
 import sympy as sp
 
-# Define the variables
-a, b, c, d, e = sp.symbols('a b c d e')
+def solution_station_7():
+    # Define the variables
+    a, b, c, d, e = sp.symbols('a b c d e')
 
-# Define the equations
-eq1 = sp.Eq(b * c + e + d, 3.5)
-eq2 = sp.Eq(a * c + d, 19)
-eq3 = sp.Eq(d+b+e, 6.5)
-eq4 = sp.Eq(e*b*c, -2)
-eq5 = sp.Eq(c+e+a, 7.5)
-eq6 = sp.Eq(e*b, -0.5)
+    # Define the equations
+    eq1 = sp.Eq(b * c + e + d, 3.5)
+    eq2 = sp.Eq(a * c + d, 19)
+    eq3 = sp.Eq(d+b+e, 6.5)
+    eq4 = sp.Eq(e*b*c, -2)
+    eq5 = sp.Eq(c+e+a, 7.5)
+    eq6 = sp.Eq(e*b, -0.5)
 
 
-# Solve the equations
-solutions = sp.solve((eq1, eq2, eq3, eq4, eq5, eq6), (a, b, c, d, e))
+    # Solve the equations
+    solution = sp.solve((eq1, eq2, eq3, eq4, eq5, eq6), (a, b, c, d, e))
 
-#  Print the solutions
-print(solutions)
+    return(solution)
+
+print(solution_station_7())
