9f2aa6ae9fb83e109becdad5e4cebfe543da14c3
josefinem12 <josymertens11@gmail.com>
2024-09-03 13:50:21 +0200

    Update station3.py

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -5,4 +5,4 @@ test_numbers = [99, 52, 72, 42, 43, 21, 53, 72, 31, 64, 51]
 results = {num: solution_station_3(num) for num in test_numbers}
 print(results)
 
-print(solution_station_3())
+print(solution_station_3(62340))
